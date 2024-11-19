0cca7171e0490c3cb5ee8a5c3f13c5b07ef9d130
keicreate <keicreate20240704@gmail.com>
2024-11-19 19:02:59 +0900

    phpを記述しました。

--- wp/src/single.php
+++ wp/src/single.php
@@ -7,130 +7,51 @@
         <div class="p-single">
           <!-- p-single__breadcrumbs -->
           <div class="p-single__breadcrumbs">
-            <span
-              ><a href="#" class="home"><span>HOME</span></a>
-            </span>
-            &gt;
-            <span
-              ><a href="#"
-                ><span
-                  >未経験から始める！ヨガスタジオ受付スタッフのリアルな1日</span
-                ></a
-              >
-            </span>
+            <?php if (function_exists("bcn_display")): ?>
+            <?php bcn_display(); ?>
+            <?php endif; ?>
           </div>
           <!-- /p-single__breadcrumbs -->
           <div class="p-single__inner">
             <div class="p-single__content">
               <!-- p-single__index-wrap -->
               <div class="p-single__index-wrap p-single__index-wrap--pc">
-                <p class="p-single__index-head">目次</p>
-                <div class="p-single__index-body">
-                  <ul class="p-single__index-list">
-                    <li class="p-single__index-item">
-                      <a href="#" class="p-single__index-link"
-                        >1. スタジオ受付スタッフの一年目の一日に密着してみた</a
-                      >
-                      <ul class="p-single__index-sublist">
-                        <li class="p-single__index-subitem">
-                          <a href="#" class="p-single__index-sublink"
-                            >朝、心地よい空間づくりからスタート</a
-                          >
-                        </li>
-                        <li class="p-single__index-subitem">
-                          <a href="#" class="p-single__index-sublink"
-                            >1.笑顔で迎える「はじめまして」の瞬間</a
-                          >
-                        </li>
-                        <li class="p-single__index-subitem">
-                          <a href="#" class="p-single__index-sublink"
-                            >2.お昼休憩でリフレッシュ！チームで過ごす時間も大切に</a
-                          >
-                        </li>
-                        <li class="p-single__index-subitem">
-                          <a href="#" class="p-single__index-sublink"
-                            >3.支えるインストラクター、見守るスタジオスタッフ</a
-                          >
-                        </li>
-                        <li class="p-single__index-subitem">
-                          <a href="#" class="p-single__index-sublink">
-                            4.1日の締めくくり、今日の気づきを共有するフ</a
-                          >
-                        </li>
-                      </ul>
-                    </li>
-                  </ul>
+                <?php echo do_shortcode('[ez-toc]'); ?>
                 </div>
               </div>
               <!-- /p-single__index-wrap -->
               <!-- p-single__column-wrap -->
               <div class="p-single__column-wrap">
                 <div class="p-single__column-head">
-                  <time datetime="2024-01-01" class="p-single__column-date"
-                    >2024.01.01</time
+                  <time datetime="<?php the_time( 'c' ); ?>" class="p-single__column-date"
+                    ><?php the_time( 'Y.m.d' ); ?></time
                   >
                   <h1 class="p-single__column-title">
-                    未経験から始める！<br />
-                    ヨガスタジオ受付スタッフのリアルな1日
+                    <?php the_title(); ?>
                   </h1>
                   <div class="p-single__column-thumbnail">
-                    <img
-                      src="./assets/img/single/single-photo-01.webp"
-                      alt=""
-                      width="1424"
-                      height="948"
-                    />
+                    <?php the_post_thumbnail(); ?>
                   </div>
                 </div>
                 <div class="p-single__column-body">
-                  <h2>スタジオ受付スタッフの一年目の一日に密着してみた</h2>
-                  <p>
-                    ヨガスタジオでの仕事に興味はあるけれど、「未経験でも大丈夫かな？」と不安に思う方も多いはず。そこで、今回はヨガ業界未経験からスタートしたスタッフが日々どのように働き、やりがいや成長を感じているか、リアルな1日の流れを紹介します！
-                  </p>
-                  <!-- custom-HTML -->
-                  <div class="p-single__index-wrap p-single__index-wrap--sp">
-                    <p class="p-single__index-head">目次</p>
-                    <div class="p-single__index-body">
-                      <ul class="p-single__index-list">
-                        <li class="p-single__index-item">
-                          <a href="#" class="p-single__index-link"
-                            >1.
-                            スタジオ受付スタッフの一年目の一日に密着してみた</a
-                          >
-                          <ul class="p-single__index-sublist">
-                            <li class="p-single__index-subitem">
-                              <a href="#" class="p-single__index-sublink"
-                                >朝、心地よい空間づくりからスタート</a
-                              >
-                            </li>
-                            <li class="p-single__index-subitem">
-                              <a href="#" class="p-single__index-sublink"
-                                >1.笑顔で迎える「はじめまして」の瞬間</a
-                              >
-                            </li>
-                            <li class="p-single__index-subitem">
-                              <a href="#" class="p-single__index-sublink"
-                                >2.お昼休憩でリフレッシュ！チームで過ごす時間も大切に</a
-                              >
-                            </li>
-                            <li class="p-single__index-subitem">
-                              <a href="#" class="p-single__index-sublink"
-                                >3.支えるインストラクター、見守るスタジオスタッフ</a
-                              >
-                            </li>
-                            <li class="p-single__index-subitem">
-                              <a href="#" class="p-single__index-sublink">
-                                4.1日の締めくくり、今日の気づきを共有するフ</a
-                              >
-                            </li>
-                          </ul>
-                        </li>
-                      </ul>
+                  <?php the_content(); ?>
+                </div>
+                <div class="p-single__pagination p-single-pagination">
+                    <div class="p-single-pagination__prev">
+                      <?php if (get_previous_post()) : ?>
+                        <?php previous_post_link('%link', '<span>Prev<span>'); ?>
+                      <?php endif; ?>
                     </div>
-                    <!-- custom-HTML -->
-                    <h3>朝、心地よい空間づくりからスタート</h3>
-                    <p>スタッフの1日は、まずスタジオの清掃や準備から始まります。心地よい音楽をかけ、整えられた空間にすることで、自分自身も落ち着くし、お客様も気持ちよくスタートできると感じています。ヨガスタジオでの清掃は、単なる作業ではなく、リラックスした環境を提供する大切な役割。整った空間は、次の時間へと気持ちを前向きにしてくれます。</p>
-                  </div>
+
+              <div class="p-single-pagination__archive">
+                <a href="<?php echo esc_url(home_url('/')); ?>/blog">一覧へ戻る</a>
+              </div>
+
+              <div class="p-single-pagination__next">
+                <?php if (get_next_post()) : ?>
+                  <?php next_post_link('%link', '<span>Next<span>'); ?>
+                <?php endif; ?>
+              </div>
                 </div>
               </div>
               <!-- /p-single__column-wrap -->
