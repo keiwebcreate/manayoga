801e037133456697dd408f7fc009a416c7e650ce
keicreate <keicreate20240704@gmail.com>
2024-11-25 18:11:10 +0900

    functions.phpに追記しました。

--- wp/src/archive.php
+++ wp/src/archive.php
@@ -1,5 +1,127 @@
 <?php get_header(); ?>
 
+<!-- l-main -->
+    <main class="l-main">
+      <!-- l-blog -->
+      <section class="l-blog">
+        <div class="p-blog">
+          <!-- p-blog__head -->
+          <h2 class="p-blog__head">
+            <span class="p-blog__head-en">Blog</span>
+            <span class="p-blog__head-ja">ブログ</span>
+          </h2>
+          <!-- /p-blog__head -->
+          <!-- p-blog__breadcrumbs -->
+          <div class="p-blog__breadcrumbs">
+            <span
+              ><a href="#" class="home"><span>HOME</span></a>
+            </span>
+            &gt;
+            <span
+              ><a href="#"><span>Blog</span></a>
+            </span>
+          </div>
+          <!-- /p-blog__breadcrumbs -->
+          <div class="p-blog__inner">
+            <div class="p-blog__content">
+              <!-- p-blog__list -->
+              <ul class="p-blog__list">
+                <li class="p-blog__item">
+                  <a href="#" class="p-blog__link">
+                    <article class="p-blog__card p-blog-card">
+                      <div class="p-blog-card__thumbnail">
+                        <img src="./assets/img/blog/blog-photo-01.webp" alt="ブログのサムネイル" width="760" height="400">
+                      </div>
+                      <time datetime="2023-03-01" class="p-blog-card__date">2023.03.01</time>
+                      <h3 class="p-blog-card__title">好きなことを仕事にする楽しさと成長の秘訣</h3>
+                      <ul class="p-blog-card__tag-list">
+                        <li class="p-blog-card__tag-item">#やりがい</li>
+                        <li class="p-blog-card__tag-item">#キャリア</li>
+                        <li class="p-blog-card__tag-item">#ストーリー</li>
+                        <li class="p-blog-card__tag-item">#キャリア</li>
+                      </ul>
+                    </article>
+                  </a>
+                </li>
+                <li class="p-blog__item">
+                  <a href="#" class="p-blog__link">
+                    <article class="p-blog__card p-blog-card">
+                      <div class="p-blog-card__thumbnail">
+                        <img src="./assets/img/blog/blog-photo-02.webp" alt="ブログのサムネイル" width="760" height="400">
+                      </div>
+                      <time datetime="2023-03-01" class="p-blog-card__date">2023.03.01</time>
+                      <h3 class="p-blog-card__title">好きなことを仕事にする楽しさと成長の秘訣</h3>
+                      <ul class="p-blog-card__tag-list">
+                        <li class="p-blog-card__tag-item">#やりがい</li>
+                        <li class="p-blog-card__tag-item">#キャリア</li>
+                        <li class="p-blog-card__tag-item">#ストーリー</li>
+                        <li class="p-blog-card__tag-item">#キャリア</li>
+                      </ul>
+                    </article>
+                  </a>
+                </li>
+                <li class="p-blog__item">
+                  <a href="#" class="p-blog__link">
+                    <article class="p-blog__card p-blog-card">
+                      <div class="p-blog-card__thumbnail">
+                        <img src="./assets/img/blog/blog-photo-03.webp" alt="ブログのサムネイル" width="760" height="400">
+                      </div>
+                      <time datetime="2023-03-01" class="p-blog-card__date">2023.03.01</time>
+                      <h3 class="p-blog-card__title">親子でヨガのススメ。</h3>
+                      <ul class="p-blog-card__tag-list">
+                       <li class="p-blog-card__tag-item">#やりがい</li>
+                        <li class="p-blog-card__tag-item">#キャリア</li>
+                        <li class="p-blog-card__tag-item">#ストーリー</li>
+                        <li class="p-blog-card__tag-item">#キャリア</li>
+                      </ul>
+                    </article>
+                  </a>
+                </li>
+                <li class="p-blog__item">
+                  <a href="#" class="p-blog__link">
+                    <article class="p-blog__card p-blog-card">
+                      <div class="p-blog-card__thumbnail">
+                        <img src="./assets/img/blog/blog-photo-04.webp" alt="ブログのサムネイル" width="760" height="400">
+                      </div>
+                      <time datetime="2023-03-01" class="p-blog-card__date">2023.03.01</time>
+                      <h3 class="p-blog-card__title">好きなことを仕事にする楽しさと成長の秘訣</h3>
+                      <ul class="p-blog-card__tag-list">
+                        <li class="p-blog-card__tag-item">#やりがい</li>
+                        <li class="p-blog-card__tag-item">#キャリア</li>
+                        <li class="p-blog-card__tag-item">#ストーリー</li>
+                        <li class="p-blog-card__tag-item">#キャリア</li>
+                      </ul>
+                    </article>
+                  </a>
+                </li>
+                <li class="p-blog__item">
+                  <a href="#" class="p-blog__link">
+                    <article class="p-blog__card p-blog-card">
+                      <div class="p-blog-card__thumbnail">
+                        <img src="./assets/img/blog/blog-photo-05.webp" alt="ブログのサムネイル" width="760" height="400">
+                      </div>
+                      <time datetime="2023-03-01" class="p-blog-card__date">2023.03.01</time>
+                      <h3 class="p-blog-card__title">好きなことを仕事にする楽しさと成長の秘訣</h3>
+                      <ul class="p-blog-card__tag-list">
+                        <li class="p-blog-card__tag-item">#やりがい</li>
+                        <li class="p-blog-card__tag-item">#キャリア</li>
+                        <li class="p-blog-card__tag-item">#ストーリー</li>
+                        <li class="p-blog-card__tag-item">#キャリア</li>
+                      </ul>
+                    </article>
+                  </a>
+                </li>
+              </ul>
+              <!-- /p-blog__list -->
+              <div class="c-pagination">
 
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- /l-blog -->
+    </main>
+    <!-- /l-main -->
 
 <?php get_footer(); ?>
